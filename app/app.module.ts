e4d987312410347cf277cbdc181fa15261733bfe
Alex <yaduol@hotmail.com>
2016-11-07 17:58:33 -0800

    tow way data binding using ngModel in "todo-input.component"
    *Note: need to import "FormsModule" from "angular/forms"

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+
 import { TodoInput } from './todo-input.component';
 import { TodoList } from './todo-list.component';
 
+import { FormsModule }   from '@angular/forms';
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule,FormsModule ],
   declarations: [ AppComponent, TodoInput, TodoList ],
   bootstrap:    [ AppComponent ]
 })
--- app/todo-input.component.ts
+++ app/todo-input.component.ts
@@ -4,18 +4,21 @@ import {TodoService} from './todo.service' ;
 @Component({
     selector: 'todo-input',
     template: `<div>TodoInput here<div>
-                <input type="text" #myInput>
-                <button (click) = "onClick(myInput.value)">button</button>
+                <input type="text" [(ngModel)]="todoModel">
+                <button (click)="onClick()">button</button>
             `
 })
 
 export class TodoInput { 
+    
+    todoModel: any;
+    
     constructor(private todoService:TodoService) {
         console.log("TodoInput constructor")
     }
 
-    onClick(value: string) {
-        this.todoService.todos.push(value)
+    onClick() {
+        this.todoService.todos.push(this.todoModel)
     }
 
 
