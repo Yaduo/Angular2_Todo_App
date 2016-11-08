3211dcc56897f578e9b2bd35d80277878accdb5b
Alex <yaduol@hotmail.com>
2016-11-07 17:54:39 -0800

    simple event and reference

--- app/todo-input.component.ts
+++ app/todo-input.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'todo-input',
-    template: '<div>TodoInput here<div>'
+    template: `<div>TodoInput here<div>
+                <input type="text" #myInput>
+                <button (click) = "onClick(myInput.value)"><button>
+            `
 })
 
 export class TodoInput { 
@@ -10,5 +13,10 @@ export class TodoInput {
     constructor() {
         console.log("TodoInput constructor")
     }
+
+    onClick(str: string) {
+        alert(str)
+    }
+
 
 }
